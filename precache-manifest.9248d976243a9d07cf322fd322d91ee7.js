3956e9332b86f5febd3f531974c2fbda912bdacd
thao8595 <50005198+thao8595@users.noreply.github.com>
2019-10-23 10:42:44 +0700

    Updates

---
+++ precache-manifest.9248d976243a9d07cf322fd322d91ee7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6bcad8a8887c8e0787252af01b4ae3",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "54d54ac50a52ff1553a3",
+    "url": "/react-quiz/static/css/main.fa5ae90a.chunk.css"
+  },
+  {
+    "revision": "a137ee50a32861575b53",
+    "url": "/react-quiz/static/js/2.8519ad6c.chunk.js"
+  },
+  {
+    "revision": "54d54ac50a52ff1553a3",
+    "url": "/react-quiz/static/js/main.14c9342e.chunk.js"
+  },
+  {
+    "revision": "c1dfdcfebe06c9f8cfc1",
+    "url": "/react-quiz/static/js/runtime-main.3cfc9fc6.js"
+  }
+]);
